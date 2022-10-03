f98cf192545d966b6cd62e2140e7080460e630c7
caoster <caoster@icloud.com>
2022-10-03 22:15:53 +0800

    check

---
+++ check.py
@@ -0,0 +1,12 @@
+a = input()
+with open("input.in") as input_file:
+    with open(a) as output_file:
+        b = input_file.read()
+        c = output_file.read()
+
+correct = 0
+for i in range(10000):
+    if b[i] == c[i]:
+        correct += 1
+
+print(correct)
